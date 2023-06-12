3a435fea5b4e06ad5d52b6a43234f60f31532dae
Fef97 <federicakr97@gmail.com>
2023-06-12 15:55:55 +0200

    scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader',
+
+}
+
+</script>
+
+<template>
+    <div>
+        ciao
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables.scss' as *;
+</style>
