05f76e2894032a55760ea981fc0538576a458672
Fef97 <federicakr97@gmail.com>
2023-06-13 22:51:59 +0200

    aggiunte immagini sezione servizi

--- src/components/section/AppProducts.vue
+++ src/components/section/AppProducts.vue
@@ -6,7 +6,25 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <section>
+        <!-- container sezione prodotti -->
+        <div class="products">
+            <span>WE HAVE YOU COVERED</span>
+            <h1>Avada Grooming Products</h1>
+
+            <!-- riga  prodotti-->
+            <div class="row">
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+            </div>
+
+            <button></button>
+        </div>
+    </section>
+</template>
 
 <style lang="scss" scoped>
 @use '../../styles/general.scss' as *;
